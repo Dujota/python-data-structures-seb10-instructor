1a2e576e5d4df387b8f46eff2cec737c080d3b10
Denis S Dujota <denisdujota@gmail.com>
2024-08-26 16:08:13 +0300

    finish data structures:

--- data_structures.py
+++ data_structures.py
@@ -69,3 +69,84 @@ for key, val in student.items():
     # favorite_integer is 5
     # age is 25
 print('=====================')
+
+# LISTS
+colors = ['red', 'blue', 'green']
+
+first = colors[0]
+print(first)  # prints: red
+last = colors[len(colors) - 1]
+last = colors[-1]
+
+colors[-1] = "purple"
+print(colors[-1])  # prints: purple
+
+# ADDING TO LISTS
+# arr.push() in js --> arr.append()
+colors.append('yellow')
+
+# Append multiple items to a list
+colors.extend(['black', 'white'])
+
+colors.insert(1, 'teal')
+print(colors)
+
+# REMOVING FROM LISTS
+# arr.pop() in js --> arr.pop(index)
+
+blue = colors.pop(2)
+print(colors)
+print(blue)
+
+colors[3] = 'black'
+# remove by value
+colors.remove('black')
+print(colors)
+
+# clean an array of its items
+colors.clear()
+
+#looping through a list
+arr = ['red', 'green', 'blue']
+
+for item in arr:
+   print(item)
+
+# 0:red 1:green 2:blue
+#[(0,val), (1,val), (2,val)]
+for index, item in enumerate(arr):
+    print(index, item)
+
+# TUPLES - basically a list, that cannot be changed
+hello_tuple = ('Hello')
+# this will not create a tuple
+print(type(hello_tuple))
+# prints: <class 'str'>
+
+hello_tuple = ('Hello',)
+# or just the following (remember parenthesis are not required)
+hello_tuple = 'Hello',
+print(type(hello_tuple))
+# prints: <class 'tuple'>
+
+colors_tuple = ('red', 'green', 'blue')
+print(colors_tuple[1])
+# prints: green
+print(colors_tuple.index('blue'))
+print('=====================')
+
+
+for color in colors_tuple:
+   print(color)
+print('=====================')
+# 0:red 1:green 2:blue
+#[(0,val), (1,val), (2,val)]
+for index, item in enumerate(colors_tuple):
+    print(index, item)
+
+
+r,g,b = colors_tuple
+print('=====================')
+print(r)
+print(g)
+print(b)
